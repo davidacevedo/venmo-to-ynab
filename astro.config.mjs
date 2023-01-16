5991d353ba3632d90b46b9dce17f94e66dd698e4
David Acevedo <davidacevedo@users.noreply.github.com>
2023-01-16 12:23:36 -0800

    build: remove site information

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://davidacevedo.github.io/venmo-to-ynab",
-  base: "/venmo-to-ynab",
   vite: {
     plugins: [nodePolyfills({})],
   },
